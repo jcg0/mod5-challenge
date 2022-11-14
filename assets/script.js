6fa72be940b4b2f39f803928649d2f500309782c
jcg0 <jg.awsprod@gmail.com>
2022-11-14 13:17:37 -0500

    created saveBtn event that saves the localStorage on the element with the description class

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-const saveButtonEl = $("button");
-const dailyEvent = [];
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -11,15 +9,19 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  saveButtonEl.on("click", function () {
+  $(".saveBtn").on("click", () => {
     console.log("clicked");
+    const text = $(".description").val();
+    localStorage.setItem("description", text);
+    // $(this).localStorage().setItem("hour", hour);
+    // $(event.currentTarget).localStorage().setItem()
   });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  $();
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
